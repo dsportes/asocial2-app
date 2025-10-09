1d4939160dc946343880936aa19f6aa11a1c6467
Daniel Sportes <daniel@sportes.fr>
2025-10-09 17:29:02 +0200

    data-store en dev

--- src/src-fw/document.ts
+++ src/src-fw/document.ts
@@ -5,6 +5,8 @@ import { DocType } from './doctypes'
 export class Document {
   _clazz: string
   _dt: DocType
+  _pk: string
+  v: number
 
   static classes: Object
   static setClasses (arg: Object) { Document.classes = arg }
@@ -14,13 +16,15 @@ export class Document {
   static compile (clazz: string, data: Uint8Array) {
     const cl = Document.classes[clazz]
     if (!cl) return null
-    const obj = new cl() as Document
-    obj['_clazz'] = clazz
-    obj['_dt'] = DocType.get(clazz)
+    const dt = DocType.get(clazz)
+    const doc: Document = new cl() as Document
+    doc._clazz = clazz
+    doc._dt = DocType.get(clazz)
     const d = data ? decode(data) : {}
-    for(const f in d) obj[f] = d[f]
-    obj.compile()
-    return obj
+    for(const f in d) doc[f] = d[f]
+    doc._pk = doc._dt.pkValue(doc)
+    doc.compile()
+    return doc
   }
 
   compile() { }
