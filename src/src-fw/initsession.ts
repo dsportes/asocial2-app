2b38b36f186bb6a2b58c554c22c36dee8ef812d1
Daniel Sportes <daniel@sportes.fr>
2025-12-12 18:12:15 +0100

    Détail

--- src/src-fw/initsession.ts
+++ src/src-fw/initsession.ts
@@ -3,36 +3,33 @@ import stores from '../stores/all'
 import { IDB } from './idb'
 import { Subscription } from'./document'
 import { SetSubscription } from './operations'
-import { modes } from '../stores/session-store'
 
 type age = {
   lsd: number // jour (EPOCH) de dernière synchro aboutie
 }
 
 export const sessionPhase0 = async (
-  dbReset: boolean, dbName: string, context: Object ) => {
+  dbReset: boolean, context: Object ) => {
   
   // Acquisition des souscriptions et documents
   const session = stores.session
   const dataSt = stores.data
   const config = stores.config
-
-  if (session.hasIDB) session.setDbName(dbName)
   
-  if (session.mode === modes.SYNC && dbReset) 
+  if (session.hasIDB && dbReset) 
     await IDB.delete(session.dbName)
 
   let idb = session.hasIDB ? await IDB.open() : null
   let integral = true
   const jourj = Math.floor(Date.now() / 86400000)
 
-  if (session.mode === modes.SYNC) {
+  if (session.hasIDB) {
     const age = await idb.getState('age') as age
     integral = !age.lsd || (jourj - age.lsd) > config.K.SYNCINCRNBD
   }
 
   // Inscription en data-store des defs de toutes les sousciptions
-  if (session.mode !== modes.PLANE) {
+  if (session.hasNet) {
     const morg = await idb.getSubs()
     for(const [org, mclazz] of morg) {
       for(const [clazz, subs] of mclazz)
@@ -45,7 +42,7 @@ export const sessionPhase0 = async (
   Des notifications peuvent désormais parvenir mais sont accumulées et NON traitées:
   la queue de traitement n'est ouverte qu'à la fin de la phase 0.
   */
-  if (session.mode !== modes.PLANE) {
+  if (session.hasNet) {
     const allSubs: Map<string, Subscription> = await idb.getSubscriptions()
     for(const [org, subscription] of allSubs) {
       dataSt.setSubscription(org, subscription)
@@ -53,7 +50,7 @@ export const sessionPhase0 = async (
     }
   }
 
-  if (session.mode === modes.SYNC && integral)
+  if (session.hasNet && session.hasIDB && integral)
     await idb.deleteAllDocs()
   if (session.hasIDB && !integral)
     await idb.loadAllDocs((org: string, doc: Document) => {
@@ -64,7 +61,7 @@ export const sessionPhase0 = async (
   if (session.hasNet)
     await dataSt.syncAll()
   // Marquage de l'age de l'état de synchro de IDB
-  if (session.mode === modes.SYNC)
+  if (session.hasNet && session.hasIDB)
     await idb.putState('age', { lsd: jourj })
 
   // Fin de phase 1
