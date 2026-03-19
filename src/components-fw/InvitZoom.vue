760d499b80c8b2fdcbc03e38d5bff766d475a937
Daniel Sportes <daniel@sportes.fr>
2026-03-19 12:39:48 +0100

    en chantier

--- src/components-fw/InvitZoom.vue
+++ src/components-fw/InvitZoom.vue
@@ -1,10 +1,19 @@
 <template>
 <div>
-  <div v-if="invit.isU && invit.status === 2" class="row justify-end q-gutter-sm">
-    <btn-cond 
+  <div v-if="invit.isU && invit.status === 2" 
+    class="row justify-end q-gutter-sm">
+    <btn-cond :label="$t('INVac_de')" icon="close" color="warning"
+      @ok="dialogs.decline = true"/>
+    <btn-cond :label="$t('INVac_ac')" icon="check" 
+      @ok="emit('accept', true)"/>
   </div>
-  <div v-if="sponsor && invit.status === 1" class="row justify-end q-gutter-sm">
 
+  <div v-if="sponsor && invit.status === 1" 
+    class="row justify-end q-gutter-sm">
+    <btn-cond :label="$t('INVac_re')" icon="close" color="warning"
+      @ok="dialogs.reject = true"/>
+    <btn-cond :label="$t('INVac_va')" icon="check" 
+      @ok="emit('validate', true)"/>
   </div>
 
   <div class="q-mt-sm row items-center">
@@ -56,18 +65,56 @@
     </div>
   </div>
 
+  <dialog-std0 v-model="dialogs.reject" :title="$t('INVac_rej_1')"
+    @close="txt = ''">
+    <template #hdr>
+      <div class="row justify-between">
+        <div class="titre-md text-italic">{{$t('INVac_rej_2', [min])}}
+        <btn-cond color="warning" :label="$t('INVac_re')"
+          :disable="txt.length < min"
+          @ok="dialogs.reject = false; emit('reject', txt)"/>
+      </div>
+    </template>
+    <template #default>
+      <q-input class="q-pa-xs bord1" v-model="txt" type="textarea"
+        borderless :rows="5"/>
+    </template>
+  </dialog-std0>
+
+  <dialog-std0 v-model="dialogs.decline" :title="$t('INVac_dec_1')"
+    @close="txt = ''">
+    <template #hdr>
+      <div class="row justify-between">
+        <div class="titre-md text-italic">{{$t('INVac_dec_2', [min])}}
+        <btn-cond color="warning" :label="$t('INVac_de')"
+          :disable="txt.length < min"
+          @ok="dialogs.decline = false; emit('decline', txt)"/>
+      </div>
+    </template>
+    <template #default>
+      <q-input class="q-pa-xs bord1" v-model="txt" type="textarea"
+        borderless :rows="5"/>
+    </template>
+  </dialog-std0>
+
 </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref, reactive, watch } from 'vue'
 
 import { $t, dhcool } from '../src-fw/util'
+
+const emit = defineEmits(['reject', 'decline', 'accept', 'validate'])
 
 const props = defineProps({
   invit: Object, // Objet Invitation
   sponsor: Boolean // true si le user est un SPONSOR qui PEUT valider / refuse
 })
+
+const dialogs = reactive({ reject: false, decline: false })
+
+const txt = ref('')
 
 </script>
 
